45597be1bcb0a6b718312b7a3fee7f4a3bd80ed0
Joe Heck <heckj@mac.com>
2022-02-06 14:25:05 -0800

    experiments in gestures - drag appears to be getting swallowed up by the ARView underneath, but a little hard to tell

--- ARTreeMac/ContentView.swift
+++ ARTreeMac/ContentView.swift
@@ -11,14 +11,37 @@ import Combine
 
 struct ContentView : View {
     @State var count = 0
-    @GestureState var magnifyBy = 1.0
+    @State var magValue = 1.0
+    @State var rotation = 0.0
+    @State var inclination = 0.0
+    
+    @State var isDragging = false
+    
+    @State var aTransform: Transform = Transform()
+    // I think I want something that SwiftUI "owns" memory wise - so @State or such, but I want to use that
+    // to update a reference to the camera, which doesn't exist when this all starts...
+    // So do I pass down a binding? Or create an instance of something and pass that down into
+    // ARViewContainer through its initializer?
+    
     var magnification: some Gesture {
-            MagnificationGesture()
-                .updating($magnifyBy) { currentState, gestureState, transaction in
-                    gestureState = currentState
-                    print("Current magnification: \(currentState)")
-                }
-        }
+        MagnificationGesture()
+            .onEnded { finalValue in
+                magValue = magValue * finalValue
+                print("Set mag value to \(magValue)")
+            }
+    }
+    
+    var drag: some Gesture {
+        DragGesture()
+            .onChanged { dragval in
+                self.isDragging = true
+                print("draggin: \(dragval.location), \(dragval.translation)")
+            }
+            .onEnded { dragval in
+                self.isDragging = false
+                print("drag fininshed at: \(dragval.location), \(dragval.translation)")
+            }
+    }
     
     var body: some View {
         VStack {
@@ -29,18 +52,22 @@ struct ContentView : View {
                 } label: {
                     Image(systemName: "plus")
                 }
+                Text("\(String(describing: aTransform.translation))")
             }
-            ARViewContainer()
+            ARViewContainer(cameraTransform: $aTransform)
                 .edgesIgnoringSafeArea(.all)
-                .gesture(magnification)
-
+                .highPriorityGesture(drag)
+                .simultaneousGesture(magnification)
+                .onTapGesture {
+                    print("TAPPED IN VIEW")
+                }
         }
     }
 }
 
-// some setup courtesy of example code in https://rozengain.medium.com/quick-realitykit-tutorial-programmatic-non-ar-setup-cafaf61e9884
-
 struct ARViewContainer: NSViewRepresentable {
+    
+    var cameraTransform: Binding<Transform>
     
     class ARViewCoordinator: NSObject {
         /*
@@ -58,7 +85,7 @@ struct ARViewContainer: NSViewRepresentable {
             view = nil
         }
     }
-
+    
     func makeCoordinator() -> ARViewContainer.ARViewCoordinator {
         ARViewCoordinator(self)
     }
@@ -70,27 +97,27 @@ struct ARViewContainer: NSViewRepresentable {
         // - coordinator
         // - transaction
         // - environment
-
+        
         let arView = ARView(frame: .zero)
         
         context.coordinator.view = arView
         // Set debug options
-        #if DEBUG
+#if DEBUG
         arView.debugOptions = [
-//            .showPhysics,
+            //            .showPhysics,
             .showStatistics,
-//            .none
+            //            .none
         ]
-        #endif
+#endif
         
-//        print("camera transform with default camera: \(arView.cameraTransform)")
-//        camera transform with default camera:
-//        Transform(
-//              scale: SIMD3<Float>(1.0, 1.0, 1.0),
-//              rotation: simd_quatf(real: 1.0, imag: SIMD3<Float>(0.0, 0.0, 0.0)),
-//              translation: SIMD3<Float>(0.0, 0.0, 2.0)
-//        )
-
+        //        print("camera transform with default camera: \(arView.cameraTransform)")
+        //        camera transform with default camera:
+        //        Transform(
+        //              scale: SIMD3<Float>(1.0, 1.0, 1.0),
+        //              rotation: simd_quatf(real: 1.0, imag: SIMD3<Float>(0.0, 0.0, 0.0)),
+        //              translation: SIMD3<Float>(0.0, 0.0, 2.0)
+        //        )
+        
         let cameraEntity = PerspectiveCamera()
         cameraEntity.camera.fieldOfViewInDegrees = 60
         
@@ -98,29 +125,33 @@ struct ARViewContainer: NSViewRepresentable {
         cameraAnchor.addChild(cameraEntity)
         arView.scene.addAnchor(cameraAnchor)
         
-//        print("initial camera transform: \(arView.cameraTransform)")
-//        initial camera transform: Transform(
-//            scale: SIMD3<Float>(1.0, 1.0, 1.0),
-//            rotation: simd_quatf(real: 1.0, imag: SIMD3<Float>(0.0, 0.0, 0.0)),
-//            translation: SIMD3<Float>(0.0, 0.0, 0.0))
-
-
+        //        print("initial camera transform: \(arView.cameraTransform)")
+        //        initial camera transform: Transform(
+        //            scale: SIMD3<Float>(1.0, 1.0, 1.0),
+        //            rotation: simd_quatf(real: 1.0, imag: SIMD3<Float>(0.0, 0.0, 0.0)),
+        //            translation: SIMD3<Float>(0.0, 0.0, 0.0))
+        
         let cameraDistance: Float = 3
         var currentCameraRotation: Float = 0
-        let cameraRotationSpeed: Float = 0.01
-
+        let cameraRotationSpeed: Float = 0.01 // radians per frame
+        
         arView.scene.subscribe(to: SceneEvents.Update.self) { _ in
             let x = sin(currentCameraRotation) * cameraDistance
             let z = cos(currentCameraRotation) * cameraDistance
-
+            
             let cameraTranslation = SIMD3<Float>(x, 1, z)
-            let cameraTransform = Transform(scale: .one,
+            
+            let localcameraTransform = Transform(scale: .one,
                                             rotation: simd_quatf(),
                                             translation: cameraTranslation)
-
-            cameraEntity.transform = cameraTransform
+            
+            cameraEntity.transform = localcameraTransform
             cameraEntity.look(at: .zero, from: cameraTranslation, relativeTo: nil)
-
+            // pushes the value "up" to the SwiftUI layer
+            // -- this in turn will trigger SwiftUI to invoke updateNSView() below
+            // with the context updated with context.coordinator
+            cameraTransform.wrappedValue = localcameraTransform
+            
             currentCameraRotation += cameraRotationSpeed
         }.store(in: &context.coordinator.cancellables)
         
@@ -136,14 +167,64 @@ struct ARViewContainer: NSViewRepresentable {
     
     func updateNSView(_ uiView: ARView, context: Context) {
         // Updates the state of the specified view with new information from SwiftUI.
-                
+        
+        // called every time something updates that would impact this view - in our case,
+        // every time the binding is triggered with an update.
+/*
+ Here's an example of what's included within context.environment:
+ 
+- environment: [
+    EnvironmentPropertyKey<PreferenceBridgeKey> = Value(value: Optional(SwiftUI.PreferenceBridge)),
+    EnvironmentPropertyKey<WindowRoleKey> = Optional(SwiftUI.AppWindowsController.WindowRole.main),
+    EnvironmentPropertyKey<StateRestorationContextIDKey> = Optional("SwiftUI.ModifiedContent<ARTreeMac.ContentView, SwiftUI._FlexFrameLayout>-1-AppWindow-1"),
+    EnvironmentPropertyKey<PresentedWindowToolbarStyleKey> = AnyWindowToolbarStyle(storage: SwiftUI.AnyWindowToolbarStyleStorage<SwiftUI.DefaultWindowToolbarStyle>),
+    EnvironmentPropertyKey<PresentedWindowStyleKey> = AnyWindowStyle(storage: SwiftUI.AnyWindowStyleStorage<SwiftUI.DefaultWindowStyle>),
+    EnvironmentPropertyKey<AppNavigationAuthorityKey> = Optional(SwiftUI.WeakBox<SwiftUI.AppNavigationAuthority>(base: Optional(SwiftUI.AppNavigationAuthority))),
+    EnvironmentPropertyKey<WindowsControllerKey> = Value(value: Optional(<SwiftUI.AppWindowsController: 0x6000023ac1e0>)),
+    EnvironmentPropertyKey<SceneStorageValuesKey> = Optional(SwiftUI.WeakBox<SwiftUI.SceneStorageValues>(base: Optional(SwiftUI.SceneStorageValues))),
+    EnvironmentPropertyKey<StoreKey<SceneBridge>> = Optional(SceneBridge: window = Optional(<SwiftUI.SwiftUIWindow: 0x12983ed80>)),
+    EnvironmentPropertyKey<FocusScopesKey> = [SwiftUI.Namespace.ID(id: 744)],
+    EnvironmentPropertyKey<AccessibilityRequestFocusKey> = AccessibilityRequestFocusAction(onAccessibilityFocus: nil),
+    EnvironmentPropertyKey<FocusSystemKey> = _FocusSystem(onResetToDefault: Optional((Function))),
+    EnvironmentPropertyKey<ResetFocusKey> = ResetFocusAction(onReset: Optional((Function))),
+    EnvironmentPropertyKey<FocusBridgeKey> = WeakBox<FocusBridge>(base: Optional(SwiftUI.FocusBridge)),
+    EnvironmentPropertyKey<PresentationModeKey> = Binding<PresentationMode>(transaction: SwiftUI.Transaction(plist: []), location: SwiftUI.LocationBox<SwiftUI.FunctionalLocation<SwiftUI.PresentationMode>>, _value: SwiftUI.PresentationMode(isPresented: true)),
+    EnvironmentPropertyKey<EmphasizedKey> = false, EnvironmentPropertyKey<UndoManagerKey> = Optional(<NSUndoManager: 0x6000023debc0>),
+    EnvironmentPropertyKey<AccentColorKey> = Optional(sRGB IEC61966-2.1 colorspace 0 0.478431 1 1),
+    EnvironmentPropertyKey<BackgroundMaterialKey> = nil,
+    EnvironmentPropertyKey<ColorSchemeContrastKey> = standard,
+    EnvironmentPropertyKey<ColorSchemeKey> = light,
+    EnvironmentPropertyKey<InTouchBarKey> = false,
+    EnvironmentPropertyKey<LayoutDirectionKey> = leftToRight,
+    EnvironmentPropertyKey<AllControlsNavigableKey> = false,
+    EnvironmentPropertyKey<ControlActiveKey> = key,
+    EnvironmentPropertyKey<DisplayScaleKey> = 2.0,
+    EnvironmentPropertyKey<DisplayGamutKey> = displayP3,
+    EnvironmentPropertyKey<AccessibilityLargeContentViewerKey> = false,
+    EnvironmentPropertyKey<EnabledTechnologiesKey> = AccessibilityTechnologies(technologySet: SwiftUI.(unknown context at $1e96bad58).AccessibilityTechnologySet(rawValue: 0)),
+    EnvironmentPropertyKey<AccessibilityButtonShapesKey> = false,
+    EnvironmentPropertyKey<AccessibilityPrefersCrossFadeTransitionsKey> = false,
+    EnvironmentPropertyKey<AccessibilityInvertColorsKey> = false,
+    EnvironmentPropertyKey<AccessibilityReduceMotionKey> = false,
+    EnvironmentPropertyKey<AccessibilityReduceTransparencyKey> = false,
+    EnvironmentPropertyKey<AccessibilityDifferentiateWithoutColorKey> = false,
+    EnvironmentPropertyKey<ReduceDesktopTintingKey> = false,
+    EnvironmentPropertyKey<SystemAccentValueKey> = multicolor,
+    EnvironmentPropertyKey<TimeZoneKey> = America/Los_Angeles (fixed (equal to current)),
+    EnvironmentPropertyKey<CalendarKey> = gregorian (current),
+    EnvironmentPropertyKey<LocaleKey> = en_US (current),
+    EnvironmentPropertyKey<ScenePhaseKey> = active,
+    EnvironmentPropertyKey<TimeZoneKey> = America/Los_Angeles (fixed (equal to current)),
+    EnvironmentPropertyKey<CalendarKey> = gregorian (current), EnvironmentPropertyKey<LocaleKey> = en_US (current)])
+*/
         // Context includes:
         // - coordinator
         // - transaction
         // - environment
-        print("updateNSView invoked")
-        print("- transaction: \(context.transaction))")
-        print("- environment: \(context.environment))")
+//        print("updateNSView invoked")
+//                print("- transaction: \(context.transaction))")
+//        print("- environment: \(context.environment))")
+//        print("")
     }
     
 }
