065f533c71e018c5a414292722f06a4398ae3504
Joe Heck <heckj@mac.com>
2022-02-07 15:59:50 -0800

    progress! Movement, and correct rotations.

--- ARTreeMac/CameraControlARView.swift
+++ ARTreeMac/CameraControlARView.swift
@@ -8,7 +8,7 @@
 import RealityKit
 import Cocoa
 
-@objc public class CameraControlARView: ARView {
+@objc public class CameraControlARView: ARView, ObservableObject {
 
     public enum MotionMode: Int {
         case arcball
@@ -28,12 +28,66 @@ import Cocoa
     // - maginification (increase = zoom in) interpretted as shortening the radius to the target location, and
     // zoom out does the reverse. Definitely clamp to a minimum of zero radius, and potentially want to have a
     // lower set limit not to come earlier based on a collision boundary for any target object and maybe some padding.
-
+    
+    /// The mode in which the camera is controlled by keypresses and/or mouse and gesture movements.
     var motionMode: MotionMode
+    /// The target for the camera when in arcball mode.
+    var arcballTarget: simd_float3
+    /// The angle of inclination of the camera when in arcball mode.
+    var inclinationAngle: Float
+    /// The angle of rotation of the camera when in arcball mode.
+    var rotationAngle: Float
+    /// The camera's orbital distance from the target when in arcball mode.
+    var radius: Float
+    /// The speed at which keypresses change the angles of inclination or rotation.
+    ///
+    /// This view doubles the speed valuewhen the key is held-down.
+    var keyspeed: Float
+    /// A reference to the camera anchor entity for moving, or reading the location values, for the camera.
+    var cameraAnchor: AnchorEntity
     
     required init(frame frameRect: NSRect) {
         motionMode = .arcball
+        arcballTarget = simd_float3(0,0,0)
+        inclinationAngle = 0
+        rotationAngle = 0
+        radius = 2
+        cameraAnchor = AnchorEntity(world: .zero)
+        keyspeed = 0.01
         super.init(frame: frameRect)
+        
+        let cameraEntity = PerspectiveCamera()
+        cameraEntity.camera.fieldOfViewInDegrees = 60
+        cameraAnchor.addChild(cameraEntity)
+        scene.addAnchor(cameraAnchor)
+        updateCamera()
+    }
+    
+    @MainActor func updateCamera() {
+        let translationTransform = Transform(scale: .one,
+                                        rotation: simd_quatf(),
+                                        translation: SIMD3<Float>(0, 0, radius))
+        
+//        let rotationQuaternion: simd_quatf = simd_quatf(angle: rotationAngle, axis: simd_float3(0,1,0))
+//        let rotationTransform = simd_float4x4(rotationQuaternion)
+//
+//        let inclinationQuaternion: simd_quatf = simd_quatf(angle: inclinationAngle, axis: simd_float3(0,0,1))
+//        let inclinationTransform = simd_float4x4(inclinationQuaternion)
+        
+        let combinedRotationTransform: Transform = Transform(pitch: inclinationAngle, yaw: rotationAngle, roll: 0)
+        
+//        var computedMatrix = matrix_multiply(matrix_identity_float4x4, rotationTransform)
+//        var computedMatrix = matrix_identity_float4x4 * rotationTransform
+//        computedMatrix = computedMatrix * inclinationTransform
+
+        // ORDER of operations is critical here to getting the correct transform:
+        // - identity -> rotation -> translation
+        let computed_transform = matrix_identity_float4x4 * combinedRotationTransform.matrix * translationTransform.matrix
+        
+        // This moves the camera to the right location
+        cameraAnchor.transform = Transform(matrix: computed_transform)
+        // This spins the camera AT its current location to look at a specific target location
+        cameraAnchor.look(at: arcballTarget, from: cameraAnchor.transform.translation, relativeTo: nil)
     }
         
     @MainActor required dynamic init?(coder decoder: NSCoder) {
@@ -137,21 +191,63 @@ import Cocoa
 
     override dynamic open func keyDown(with event: NSEvent) {
         print("keyDown: \(event)")
+        print("key value: \(event.keyCode)")
+        switch event.keyCode {
+            // 123 = left arrow
+            // 124 = right arrow
+            // 126 = up arrow
+            // 125 = down arrow
+            // 0 = a
+            // 1 = s
+            // 2 = d
+            // 13 = w
+            case 123, 0:
+            if event.isARepeat {
+                rotationAngle -= keyspeed * 2
+            } else {
+                rotationAngle -= keyspeed
+            }
+            updateCamera()
+            case 124, 2:
+            if event.isARepeat {
+                rotationAngle += keyspeed * 2
+            } else {
+                rotationAngle += keyspeed
+            }
+            updateCamera()
+            case 126, 13:
+            if inclinationAngle > -Float.pi/2 {
+                if event.isARepeat {
+                    inclinationAngle -= keyspeed * 2
+                } else {
+                    inclinationAngle -= keyspeed
+                }
+                updateCamera()
+            }
+            case 125, 1:
+            if inclinationAngle < Float.pi/2 {
+                if event.isARepeat {
+                    inclinationAngle += keyspeed * 2
+                } else {
+                    inclinationAngle += keyspeed
+                }
+                updateCamera()
+            }
+            default:
+                break
+        }
     }
 
     override dynamic open func keyUp(with event: NSEvent) {
-        print("keyUp: \(event)")
+        //print("keyUp: \(event)")
     }
     
     override dynamic open func magnify(with event: NSEvent) {
         print("magnify: \(event)")
     }
     
-    override dynamic open func rotate(with event: NSEvent) {
-        print("rotate: \(event)")
-    }
-//    override dynamic open func swipe(with event: NSEvent) {
-//        print("swipe: \(event)")
+//    override dynamic open func rotate(with event: NSEvent) {
+//        print("rotate: \(event)")
 //    }
 
 }
--- ARTreeMac/ContentView.swift
+++ ARTreeMac/ContentView.swift
@@ -10,72 +10,26 @@ import RealityKit
 import Combine
 
 struct ContentView : View {
-    @State var count = 0
-    @State var magValue = 1.0
-    @State var rotation = 0.0
-    @State var inclination = 0.0
-    
-    @State var isDragging = false
-    
-    @State var aTransform: Transform = Transform()
-    // I think I want something that SwiftUI "owns" memory wise - so @State or such, but I want to use that
-    // to update a reference to the camera, which doesn't exist when this all starts...
-    // So do I pass down a binding? Or create an instance of something and pass that down into
-    // ARViewContainer through its initializer?
-    
-    var magnification: some Gesture {
-        MagnificationGesture()
-            .onEnded { finalValue in
-                magValue = magValue * finalValue
-                print("Set mag value to \(magValue)")
-            }
-    }
-    
-    var drag: some Gesture {
-        DragGesture()
-            .onChanged { dragval in
-                self.isDragging = true
-                print("draggin: \(dragval.location), \(dragval.translation)")
-            }
-            .onEnded { dragval in
-                self.isDragging = false
-                print("drag fininshed at: \(dragval.location), \(dragval.translation)")
-            }
-    }
-    
+    @StateObject var arview: CameraControlARView = CameraControlARView(frame: .zero)
     var body: some View {
         VStack {
             HStack {
-                Text("My AR View on macOS: \(count)")
+                Text("My AR View on macOS")
                 Button {
-                    count += 1
+                    print("button click")
                 } label: {
                     Image(systemName: "plus")
                 }
-                Text("\(String(describing: aTransform.translation))")
             }
-            ARViewContainer(cameraTransform: $aTransform)
+            ARViewContainer(cameraARView: arview)
                 .edgesIgnoringSafeArea(.all)
-//                .highPriorityGesture(drag)
-                .simultaneousGesture(magnification)
-//                .highPriorityGesture(DragGesture(minimumDistance: 0, coordinateSpace: .local)
-//                    .onChanged({ value in
-//                        print("drag onChanged: \(value)")
-//                    })
-//                    .onEnded({ value in
-//                        print("drag onEnded: \(value)")
-//                    })
-//                )
-//                .onTapGesture {
-//                    print("TAPPED IN VIEW")
-//                }
         }
     }
 }
 
 struct ARViewContainer: NSViewRepresentable {
     
-    var cameraTransform: Binding<Transform>
+    var cameraARView: CameraControlARView
     
     class ARViewCoordinator: NSObject {
         /*
@@ -84,13 +38,10 @@ struct ARViewContainer: NSViewRepresentable {
          For example, you use a coordinator to forward target-action and
          delegate messages from your view controller to any SwiftUI views.
          */
-        var parent: ARViewContainer
-        weak var view: ARView?
-        var cancellables: Set<AnyCancellable> = []
+        var representableContainer: ARViewContainer
         
-        init(_ parent: ARViewContainer) {
-            self.parent = parent
-            view = nil
+        init(_ representableContainer: ARViewContainer) {
+            self.representableContainer = representableContainer
         }
     }
     
@@ -106,15 +57,8 @@ struct ARViewContainer: NSViewRepresentable {
         // - transaction
         // - environment
         
-        let arView = CameraControlARView(frame: .zero)
+        let arView = cameraARView
         
-        // NOTE(heckj): I think in order to sort the event handling, I'm going to have to consider subclassing
-        // ARView from RealityKit and adding my own mouse/event handling mechanisms. While some gesture capture appears
-        // to be working from above this view (in SwiftUI land), taps, drags, etc aren't being reflected. I don't know
-        // if I'm screwing up the gesture in SwiftUI, or if the ARView is consuming, and disposing, of the views before they get
-        // "up" to SwiftUI.
-        
-        context.coordinator.view = arView
         // Set debug options
 #if DEBUG
         arView.debugOptions = [
@@ -123,52 +67,7 @@ struct ARViewContainer: NSViewRepresentable {
             //            .none
         ]
 #endif
-        
-        //        print("camera transform with default camera: \(arView.cameraTransform)")
-        //        camera transform with default camera:
-        //        Transform(
-        //              scale: SIMD3<Float>(1.0, 1.0, 1.0),
-        //              rotation: simd_quatf(real: 1.0, imag: SIMD3<Float>(0.0, 0.0, 0.0)),
-        //              translation: SIMD3<Float>(0.0, 0.0, 2.0)
-        //        )
-        
-        let cameraEntity = PerspectiveCamera()
-        cameraEntity.camera.fieldOfViewInDegrees = 60
-        
-        let cameraAnchor = AnchorEntity(world: .zero)
-        cameraAnchor.addChild(cameraEntity)
-        arView.scene.addAnchor(cameraAnchor)
-        
-        //        print("initial camera transform: \(arView.cameraTransform)")
-        //        initial camera transform: Transform(
-        //            scale: SIMD3<Float>(1.0, 1.0, 1.0),
-        //            rotation: simd_quatf(real: 1.0, imag: SIMD3<Float>(0.0, 0.0, 0.0)),
-        //            translation: SIMD3<Float>(0.0, 0.0, 0.0))
-        
-        let cameraDistance: Float = 3
-        var currentCameraRotation: Float = 0
-        let cameraRotationSpeed: Float = 0.01 // radians per frame
-        
-        arView.scene.subscribe(to: SceneEvents.Update.self) { _ in
-            let x = sin(currentCameraRotation) * cameraDistance
-            let z = cos(currentCameraRotation) * cameraDistance
-            
-            let cameraTranslation = SIMD3<Float>(x, 1, z)
-            
-            let localcameraTransform = Transform(scale: .one,
-                                            rotation: simd_quatf(),
-                                            translation: cameraTranslation)
-            
-            cameraEntity.transform = localcameraTransform
-            cameraEntity.look(at: .zero, from: cameraTranslation, relativeTo: nil)
-            // pushes the value "up" to the SwiftUI layer
-            // -- this in turn will trigger SwiftUI to invoke updateNSView() below
-            // with the context updated with context.coordinator
-            cameraTransform.wrappedValue = localcameraTransform
-            
-            currentCameraRotation += cameraRotationSpeed
-        }.store(in: &context.coordinator.cancellables)
-        
+                
         // Load the "Box" scene from the "Experience" Reality File
         let boxAnchor = try! Experience.loadBox()
         
@@ -176,7 +75,6 @@ struct ARViewContainer: NSViewRepresentable {
         arView.scene.anchors.append(boxAnchor)
         
         return arView
-        
     }
     
     func updateNSView(_ uiView: ARView, context: Context) {
